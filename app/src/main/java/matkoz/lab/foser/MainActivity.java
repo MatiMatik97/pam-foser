e60079dbbebd55d0f45f852c980c30251141a637
Mateusz <matimatik97@gmail.com>
2020-11-24 18:54:31 +0100

    lab8

--- app/src/main/java/matkoz/lab/foser/MainActivity.java
+++ app/src/main/java/matkoz/lab/foser/MainActivity.java
@@ -2,24 +2,39 @@ package matkoz.lab.foser;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.preference.PreferenceManager;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonStart, buttonStop, buttonRestart;
+    private TextView textInfoService, textInfoSettings;
+    private String message;
+    private Boolean show_time, work, work_double;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonStop = (Button) findViewById(R.id.buttonStop);
+        buttonRestart = (Button) findViewById(R.id.buttonRestart);
+        textInfoService = (TextView) findViewById(R.id.textInfoServiceState);
+        textInfoSettings = (TextView) findViewById(R.id.textInfoSettings);
+
         // START
-        ((Button) findViewById(R.id.buttonStart)).setOnClickListener(new View.OnClickListener() {
+        buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 clickStart(view);
@@ -27,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // STOP
-        ((Button) findViewById(R.id.buttonStop)).setOnClickListener(new View.OnClickListener() {
+        buttonStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 clickStop(view);
@@ -35,12 +50,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // RESTART
-        ((Button) findViewById(R.id.buttonRestart)).setOnClickListener(new View.OnClickListener() {
+        buttonRestart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 clickRestart(view);
             }
         });
+
+        updateUI();
     }
 
     @Override
@@ -64,15 +81,72 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickStart(View view) {
-        Toast.makeText(this, "Start", Toast.LENGTH_SHORT).show();
+        getPreferences();
+
+        Intent startIntent = new Intent(this, MyForegroundService.class);
+        startIntent.putExtra(MyForegroundService.MESSAGE, message);
+        startIntent.putExtra(MyForegroundService.TIME, show_time);
+        startIntent.putExtra(MyForegroundService.WORK, work);
+        startIntent.putExtra(MyForegroundService.WORK_DOUBLE, work_double);
+
+        ContextCompat.startForegroundService(this, startIntent);
+
+        updateUI();
     }
 
     public void clickStop(View view) {
-        Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
+        Intent stopIntent = new Intent(this, MyForegroundService.class);
+        stopService(stopIntent);
+
+        updateUI();
     }
 
     public void clickRestart(View view) {
         clickStop(view);
         clickStart(view);
+    }
+
+    private String getPreferences() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        message = sharedPreferences.getString("message", "ForSer");
+        show_time = sharedPreferences.getBoolean("show_time", true);
+        work = sharedPreferences.getBoolean("sync", true);
+        work_double = sharedPreferences.getBoolean("double", false);
+
+        return "Message: " + message + "\n"
+                + "show_time: " + show_time.toString() + "\n"
+                + "work: " + work.toString() + "\n"
+                + "double: " + work_double.toString();
+    }
+
+    @SuppressWarnings("deprecation")
+    private boolean isMyForegroundServiceRunning() {
+        String myServiceName = MyForegroundService.class.getName();
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+
+        for (ActivityManager.RunningServiceInfo runningService : activityManager.getRunningServices(Integer.MAX_VALUE)) {
+            String runningServiceName = runningService.service.getClassName();
+            if (runningServiceName.equals(myServiceName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void updateUI() {
+        if (isMyForegroundServiceRunning()) {
+            buttonStart.setEnabled(false);
+            buttonStop.setEnabled(true);
+            buttonRestart.setEnabled(true);
+            textInfoService.setText(getString(R.string.info_service_running));
+        } else {
+            buttonStart.setEnabled(true);
+            buttonStop.setEnabled(false);
+            buttonRestart.setEnabled(false);
+            textInfoService.setText(getString(R.string.info_service_not_running));
+        }
+
+        textInfoSettings.setText(getPreferences());
     }
 }
